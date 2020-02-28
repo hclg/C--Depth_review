5efdcc4f11796431dac99c2958ca71b9370e59aa
hclg <2249121951@qq.com>
2020-02-28 13:46:31 +0800

    数组复习
    数组的各种符合类型

---
+++ 字符串、向量和数组/数组.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    const unsigned sz = 3;
+    int ial[sz] = {0, 1, 2};
+    int a2[] = {0, 1, 2};
+    int a3[5] = {0, 1, 2};
+    string a4[3] = {"hi", "bye"};
+//    int a5[2] = {0£¬1£¬2};
+    char ch[5] = "csd";
+    cout << a3[3] <<endl;
+    return 0;
+}
