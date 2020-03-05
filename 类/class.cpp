0e4da7ca3cc9d186c18b3a99d7cdd3ceb4b4768d
hclg <2249121951@qq.com>
2020-03-06 00:18:19 +0800

    类
    类的特性

--- 类/class.cpp
+++ 类/class.cpp
@@ -38,16 +38,31 @@ public:
     return contents[cursor];} //隐式内联
     inline char get(pos ht, pos wd) const; //显示内联
     screen &move(pos r, pos c); //能在之后被设为内联
+
+    screen &display(ostream &os) {
+    do_display(os); return *this;}
+    const screen &display(ostream &os) const{
+    do_display(os); return *this;}
 private:
+    void do_display(ostream &os) const {os << contents;}
     pos cursor = 0;
     pos height = 0, width = 0;
     string contents;
 };
+class window{
+private:
+    vector<screen> scr{screen(24,80,' ')};
+};
+
 inline
 screen &screen::move(pos r, pos c) {
     pos row = r*width;
     cursor = row +c;
     return *this;
+}
+char screen::get(pos r, pos c) const { //内部声明inline
+    pos row = r*width;
+    return contents[row+c];
 }
 
 
@@ -77,5 +92,14 @@ ostream &print(ostream &os, const data &item) {
 }
 
 int main() {
+
+    double a = 9;
+    const double *const c = &a;
+    const_cast<double *>(c);
+    const double &d = a;
+    const_cast<double *>(d);
+
+//    decltype((int)) b = a;
+//    cout << sizeof(decltype((int)) <<endl;
     return 0;
 }
